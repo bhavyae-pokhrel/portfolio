208b7d2b993bddffa46ac63733b90e9eda01107e
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-05-02 16:43:52 +0530

    profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,13 +29,12 @@ const Home = () => {
   return (
     <>
       <div className="container home" id="home">
-        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+         <div className="left" data-aos="fade-up-right" data-aos-duration="1000" >
           <h1 ref={typedRef}></h1>
-
           <a
             href={pdf}
             download="Resume.pdf"
-            className="btn btn-outline-warning my-3"
+            className="btn btn-outline-warning my-5"
           >Download Resume</a>
         </div>
         <div className="right">
@@ -43,7 +42,7 @@ const Home = () => {
             <img src={`/assets/${hero.imgSrc}`} alt="hero" />
           </div>
         </div>
-      </div>
+       </div>
     </>
   );
 };
